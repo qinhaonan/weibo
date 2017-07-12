808c07dc145d00ae1ec1077e9f8ba40c59c31547
qinhaonan <243272300@qq.com>
2017-07-12 19:36:01 +0800

    问专家，快速提问两个子页面布局完成，修复关于headviewadpter的bug

---
+++ weiSwift/src/main/java/com/wenming/weiswift/app/home/activity/QuestionActivity.java
@@ -0,0 +1,20 @@
+package com.wenming.weiswift.app.home.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.wenming.weiswift.R;
+import com.wenming.weiswift.app.common.base.BaseAppCompatActivity;
+
+/**
+ * Created by qhn on 2017/7/12.
+ */
+
+public class QuestionActivity extends BaseAppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question_layout);
+    }
+}
--- weiSwift/src/main/java/com/wenming/weiswift/widget/endlessrecyclerview/HeaderAndFooterRecyclerViewAdapter.java
+++ weiSwift/src/main/java/com/wenming/weiswift/widget/endlessrecyclerview/HeaderAndFooterRecyclerViewAdapter.java
@@ -1,15 +1,24 @@
 package com.wenming.weiswift.widget.endlessrecyclerview;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.wenming.weiswift.R;
+import com.wenming.weiswift.app.home.activity.ExpertActivity;
+import com.wenming.weiswift.app.home.activity.QuestionActivity;
 
 import java.util.ArrayList;
 
 import uk.co.senab.photoview.log.LoggerDefault;
+
+import static com.wenming.library.upload.UploadService.TAG;
 
 /**
  * Created by cundong on 2015/10/9.
@@ -72,6 +81,7 @@ public class HeaderAndFooterRecyclerViewAdapter extends RecyclerView.Adapter<Rec
 
     /**
      * 设置adapter
+     *
      * @param adapter
      */
     public void setAdapter(RecyclerView.Adapter<RecyclerView.ViewHolder> adapter) {
@@ -117,18 +127,20 @@ public class HeaderAndFooterRecyclerViewAdapter extends RecyclerView.Adapter<Rec
 
     /**
      * 返回第一个FoView
+     *
      * @return
      */
     public View getFooterView() {
-        return  getFooterViewsCount()>0 ? mFooterViews.get(0) : null;
+        return getFooterViewsCount() > 0 ? mFooterViews.get(0) : null;
     }
 
     /**
      * 返回第一个HeaderView
+     *
      * @return
      */
     public View getHeaderView() {
-        return  getHeaderViewsCount()>0 ? mHeaderViews.get(0) : null;
+        return getHeaderViewsCount() > 0 ? mHeaderViews.get(0) : null;
     }
 
     public void removeHeaderView(View view) {
@@ -163,13 +175,16 @@ public class HeaderAndFooterRecyclerViewAdapter extends RecyclerView.Adapter<Rec
 
         int headerViewsCountCount = getHeaderViewsCount();
         if (viewType < TYPE_HEADER_VIEW + headerViewsCountCount) {
-
+            Log.d(TAG, "onCreateViewHolder: 头部");
+            if(mHeaderViews!=null){
+            return new ViewHolder(mHeaderViews.get(viewType - TYPE_HEADER_VIEW),parent.getContext());
+            }
             return new ViewHolder(mHeaderViews.get(viewType - TYPE_HEADER_VIEW));
         } else if (viewType >= TYPE_FOOTER_VIEW && viewType < Integer.MAX_VALUE / 2) {
-
+            Log.d(TAG, "onCreateViewHolder: 尾部部");
             return new ViewHolder(mFooterViews.get(viewType - TYPE_FOOTER_VIEW));
         } else {
-
+            Log.d(TAG, "onCreateViewHolder: 中间");
             return mInnerAdapter.onCreateViewHolder(parent, viewType - Integer.MAX_VALUE / 2);
         }
     }
@@ -183,7 +198,7 @@ public class HeaderAndFooterRecyclerViewAdapter extends RecyclerView.Adapter<Rec
         } else {
             Log.d("ADFADF", "onBindViewHolder: 头");
             ViewGroup.LayoutParams layoutParams = holder.itemView.getLayoutParams();
-            if(layoutParams instanceof StaggeredGridLayoutManager.LayoutParams) {
+            if (layoutParams instanceof StaggeredGridLayoutManager.LayoutParams) {
                 Log.d("ADFADF", "onBindViewHolder: 尾");
                 ((StaggeredGridLayoutManager.LayoutParams) layoutParams).setFullSpan(true);
             }
@@ -204,7 +219,7 @@ public class HeaderAndFooterRecyclerViewAdapter extends RecyclerView.Adapter<Rec
         } else if (headerViewsCountCount <= position && position < headerViewsCountCount + innerCount) {
 
             int innerItemViewType = mInnerAdapter.getItemViewType(position - headerViewsCountCount);
-            if(innerItemViewType >= Integer.MAX_VALUE / 2) {
+            if (innerItemViewType >= Integer.MAX_VALUE / 2) {
                 throw new IllegalArgumentException("your adapter's return value of getViewTypeCount() must < Integer.MAX_VALUE / 2");
             }
             return innerItemViewType + Integer.MAX_VALUE / 2;
@@ -214,8 +229,57 @@ public class HeaderAndFooterRecyclerViewAdapter extends RecyclerView.Adapter<Rec
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(View itemView) {
+        LinearLayout ll_expert;
+        LinearLayout ll_question;
+        public ViewHolder(View itemView){
             super(itemView);
         }
+        public ViewHolder(View itemView, final Context context) {
+            super(itemView);
+            ll_expert = (LinearLayout) itemView.findViewById(R.id.ll_expert);
+            if (ll_expert != null) {
+                ll_expert.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        context.startActivity(new Intent(context, ExpertActivity.class));
+                    }
+                });
+            }
+            ll_question = (LinearLayout) itemView.findViewById(R.id.ll_question);
+            if (ll_question!=null) {
+                ll_question.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        context.startActivity(new Intent(context, QuestionActivity.class));
+                    }
+                });
+            }
+        }
     }
+//    public  class HeaderViewHolder extends RecyclerView.ViewHolder {
+//         LinearLayout ll_expert;
+//         LinearLayout ll_question;
+//
+//        public HeaderViewHolder(View itemView, final Context context) {
+//            super(itemView);
+//            ll_expert = (LinearLayout) itemView.findViewById(R.id.ll_expert);
+//            if (ll_expert != null) {
+//                ll_expert.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        context.startActivity(new Intent(context, ExpertActivity.class));
+//                    }
+//                });
+//            }
+//            ll_question = (LinearLayout) itemView.findViewById(R.id.ll_question);
+//            if (ll_question!=null) {
+//                ll_question.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        context.startActivity(new Intent(context, QuestionActivity.class));
+//                    }
+//                });
+//            }
+//        }
+//    }
 }
