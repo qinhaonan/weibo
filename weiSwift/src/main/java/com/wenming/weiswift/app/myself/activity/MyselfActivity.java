6dc7cf8ab2f4fd30ed32b034b955b25de3764e06
xun1 <243272300@qq.com>
2017-08-03 18:55:11 +0800

    我的页面初级阶段，收藏，采纳完成大部分

---
+++ weiSwift/src/main/java/com/wenming/weiswift/app/myself/activity/MyselfActivity.java
@@ -0,0 +1,8 @@
+package com.wenming.weiswift.app.myself.activity;
+
+/**
+ * Created by qhn on 2017/8/3.
+ */
+
+public class MyselfActivity {
+}
---
+++ weiSwift/src/main/java/com/wenming/weiswift/app/myself/fragment/MySelf2Fragment.java
@@ -0,0 +1,8 @@
+package com.wenming.weiswift.app.myself.fragment;
+
+/**
+ * Created by qhn on 2017/8/3.
+ */
+
+public class Myself2Fragment {
+}
